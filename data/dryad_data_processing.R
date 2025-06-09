e547c65fe139840e4cd8fb18bc430f00f45c320f
Julien Brun <brunj7@users.noreply.github.com>
2025-06-09 11:50:13 -0700

    data processing to work with the existing app

---
+++ data/dryad_data_processing.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(readr)
+library(lubridate)
+
+# Import data
+dryad_data <- read_csv("data/raw/doi_10_5061_dryad_s1rn8pkbv__v20240625/bin_eruption.csv")
+
+# Select only data for the Old Faithful and convert time to POSIX format
+data_faithful <- dryad_data |>
+  select(t, OF) |>
+  mutate(time = ymd_hm(t))
+
+# Drop 0 and compute time difference between eruptions
+data_faithful_erupt <-  data_faithful |>
+  filter(OF==1) |>
+  mutate(next_erupt = lag(OF),
+         next_erupt_time = lag(time),
+         wait_time_minutes = as.numeric(time - next_erupt_time)) |>
+  filter(wait_time_minutes < 500) # Arbitrarily remove "outliers" for better visualization
+
+# write the csv file for the app
+write_csv(data_faithful_erupt, "data/processed/old_faithful_waiting_time.csv") 
