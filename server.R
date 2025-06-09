0d34ffe30a1e8d4e575ab0798e335c10f2c69b0c
Julien Brun <brunj7@users.noreply.github.com>
2025-06-09 10:29:11 -0700

    add reference and more comments to the server code

--- server.R
+++ server.R
@@ -1,17 +1,23 @@
+# This code was adapted from the Shiny R package examples: 
+# https://github.com/rstudio/shiny-examples/tree/main/083-front-page
+
+
 function(input, output) {
-
+  # Main plot
   output$main_plot <- renderPlot({
-
+    # Histogram plot
     hist(old_faithful$eruptions,
       probability = TRUE,
       breaks = as.numeric(input$n_breaks),
       xlab = "Duration (minutes)",
       main = "Geyser eruption duration")
-
+    
+    # Adds individual observations to plot
     if (input$individual_obs) {
       rug(old_faithful$eruptions)
     }
-
+    
+    # Adds density estimates to plot
     if (input$density) {
       dens <- density(old_faithful$eruptions,
           adjust = input$bw_adjust)
