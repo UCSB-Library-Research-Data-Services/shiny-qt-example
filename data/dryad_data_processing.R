ba56b4ff4614b0b9f1587fc05da09b77229070a4
Julien Brun <brunj7@users.noreply.github.com>
2025-06-09 12:26:01 -0700

    drop columns that were not useful

--- data/dryad_data_processing.R
+++ data/dryad_data_processing.R
@@ -4,7 +4,7 @@ library(lubridate)
 
 # file path
 data_raw <- file.path("data", "raw")
-data_processed <- file.path("data", "procesed")
+data_processed <- file.path("data", "processed")
 
 # Import raw data
 dryad_data <- read_csv(file.path(data_raw, "bin_eruption.csv"))
@@ -17,9 +17,9 @@ data_faithful <- dryad_data |>
 # Drop 0 and compute time difference between eruptions
 data_faithful_erupt <-  data_faithful |>
   filter(OF==1) |>
-  mutate(next_erupt = lag(OF),
-         next_erupt_time = lag(time),
+  mutate(next_erupt_time = lag(time),
          wait_time_minutes = as.numeric(time - next_erupt_time)) |>
+  select(-OF) |>   # remove this columns as not needed anymore
   filter(wait_time_minutes < 500) # Arbitrarily remove "outliers" for better visualization
 
 # write the csv file for the app
